71f2d276480f175bc5dca04c4a80cbfc6bff83f8
vahid <vahid4134@gmail.com>
2026-03-14 22:41:28 +0100

    add password support for default user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 docker_run="docker run"
-docker_run="$docker_run -d -p 8123:8123 -p9000:9000 clickhouse/clickhouse-server:$INPUT_VERSION"
+docker_run="$docker_run -d -p 8123:8123 -p9000:9000"
+
+if [ -n "$INPUT_PASSWORD" ]; then
+  docker_run="$docker_run -e CLICKHOUSE_PASSWORD=$INPUT_PASSWORD"
+fi
+
+docker_run="$docker_run clickhouse/clickhouse-server:$INPUT_VERSION"
 
 sh -c "$docker_run"
